e2345471d30c37a54f1ffd75e05c2a9581ad4f70
Tanuki <redfox7237580@gmail.com>
2025-08-30 16:40:36 -0500

    Solución
    Estructuración del ejercicio propuesto en la diapositiva.

---
+++ Calculadora/src/Moldelo/Persona.java
@@ -0,0 +1,53 @@
+//Andres Felipe Castaño - 20231578079
+
+package Moldelo;
+
+import java.util.Calendar;
+
+public class Persona {
+    private String id, nombre;
+    private int aa;
+
+    public Persona(String id, String nombre, int aa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.aa = aa;
+    }
+
+    public Persona() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAa() {
+        return aa;
+    }
+
+    public void setAa(int aa) {
+        this.aa = aa;
+    }
+    
+    @Override
+    public String toString(){
+        return "\nId:\t" + getId() + "\nNombre:\t" + getNombre() + "\nAño de nacimento:\t" + getAa();
+    }
+    
+    public int edad(){
+        Calendar fecha = Calendar.getInstance();
+        return fecha.get(Calendar.YEAR)-this.aa;
+    }
+}
